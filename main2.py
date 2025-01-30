dda93b9ca30f46c4826b9eb78cf62c4c14d8d7fc
Paxmakos <pakostasip@gmail.com>
2025-01-30 18:47:43 +0100

    moze ostatni

--- main2.py
+++ main2.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 import json
 from consts import *
 import threading
+import time
 
 # Plik do przechowywania danych o parkowaniu
 PARKING_DATA_FILE = "parking_data.json"
@@ -27,11 +28,12 @@ def disconnectFromBroker():
 
 def processMessage(client, userdata, message):
     message_decoded = str(message.payload.decode("utf-8"))
+    print(message_decoded)
 
     message_parsed = message_decoded.split(":")
     entrance = True
 
-    if message_parsed.__contains__(EXIT_MESSAGE_1):
+    if message_decoded.__contains__(EXIT_MESSAGE_1):
         entrance = False
 
     handle_card_read(message_parsed[-1], entrance)
@@ -61,17 +63,20 @@ def handle_card_read(uid, entrance):
     if entrance:
         if str(uid) in parking_data:
             client.publish(BASE_TO_GATE_CANAL, SECOND_ENTRY_CODE)
+            client.loop_write()
             messagebox.showerror("Błąd", "Karta już zarejestrowana.")
             
         else:
             parking_data[str(uid)] = {"entry_time": time_stamp}
             save_parking_data(parking_data)
             client.publish(BASE_TO_GATE_CANAL, WELCOME_CODE)
+            client.loop_write()
             messagebox.showinfo("Sukces", f"Karta {uid} zarejestrowana przy wjeździe o {time_stamp}.")
             
     else:
         if str(uid) not in parking_data:
             client.publish(BASE_TO_GATE_CANAL, SECOND_EXIT_CODE)
+            client.loop_write()
             messagebox.showerror("Błąd", "Karta nieznaleziona.")
             
         else:
@@ -81,6 +86,7 @@ def handle_card_read(uid, entrance):
             payment = round((duration - 60) * 0.5, 2)
             if duration <= 60:  # Pierwsza godzina darmowa
                 client.publish(BASE_TO_GATE_CANAL, GOODBYE_CODE)
+                client.loop_write()
                 messagebox.showinfo("Czas postoju", f"Czas postoju: {duration:.2f} minut. Karta {uid}. Pierwsza godzina jest darmowa.")
                 del parking_data[str(uid)]
                 save_parking_data(parking_data)
@@ -88,12 +94,14 @@ def handle_card_read(uid, entrance):
             else:
                 if not messagebox.askyesno("Wyjazd", f"Czas postoju: {duration:.2f} minut. Do zapłaty: {payment:.2f} zł. Czy zapłacono?"):
                     client.publish(BASE_TO_GATE_CANAL, PAYMENT_CODE)
+                    client.loop_write()
                     messagebox.showinfo("Zapłać",f"Aby wyjechać musisz zapłacić: {payment:.2f} zł.")
                     
                 else:
                     del parking_data[str(uid)]
                     save_parking_data(parking_data)
                     client.publish(BASE_TO_GATE_CANAL, GOODBYE_CODE)
+                    client.loop_write()
                 #messagebox.showinfo("Wyjazd", f"Czas postoju: {duration:.2f} minut. Do zapłaty: {payment:.2f} zł. Aby zapłacić kliknij Ok")
             #del parking_data[str(uid)]
             #save_parking_data(parking_data)
@@ -116,6 +124,8 @@ def start_receiver():
 if __name__ == "__main__":
     # Wątki dla niezależnego działania funkcji
     receiver_thread = threading.Thread(target=start_receiver, daemon=True)
+    message_thread = threading.Thread(target=start_receiver, daemon=True)
+
 
     receiver_thread.start()
 
